f9d5a01fce85e6e8f05918931df73bcc3a77e188
Albert Chen <albert.cht@gmail.com>
2017-11-11 19:56:37 +0800

    fix getApplication(), the application instance won't be \Illuminate\Contracts\Foundation\Application after lumen 5.2.

--- src/Server/Application.php
+++ src/Server/Application.php
@@ -11,7 +11,7 @@
 
 namespace HuangYi\Http\Server;
 
-use Illuminate\Contracts\Foundation\Application as ApplicationContract;
+use Illuminate\Container\Container;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
@@ -95,7 +95,7 @@ class Application
      */
     public function getApplication()
     {
-        if (! $this->application instanceof ApplicationContract) {
+        if (! $this->application instanceof Container) {
             $this->application = $this->loadApplication();
         }
 
