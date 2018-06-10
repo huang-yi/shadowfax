63977fbac9f30a08bbd35400b1ae864ed50bcc9d
Huang Yi <coodeer@163.com>
2018-06-10 23:24:21 +0800

    MessageTest

---
+++ tests/Websocket/Message/MessageTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace HuangYi\Swoole\Tests\Websocket\Message;
+
+use HuangYi\Swoole\Websocket\Message\Message;
+use PHPUnit\Framework\TestCase;
+
+class MessageTest extends TestCase
+{
+    /**
+     * @var \HuangYi\Swoole\Websocket\Message\Message
+     */
+    protected $message;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->message = new Message('test', ['hello world']);
+    }
+
+    public function testGetEvent()
+    {
+        $this->assertEquals('test', $this->message->getEvent());
+    }
+
+    public function testGetData()
+    {
+        $this->assertEquals(['hello world'], $this->message->getData());
+    }
+
+    public function testSocketId()
+    {
+        $this->message->setSocketId(1);
+
+        $this->assertEquals(1, $this->message->getSocketId());
+    }
+
+    public function testToArray()
+    {
+        $this->assertEquals(['event' => 'test', 'data' => ['hello world']], $this->message->toArray());
+    }
+
+    public function testToJson()
+    {
+        $this->assertEquals('{"event":"test","data":["hello world"]}', $this->message->toJson());
+    }
+
+    public function testToString()
+    {
+        $this->assertEquals('{"event":"test","data":["hello world"]}', (string) $this->message);
+    }
+}
