8e1211be78bcbe205ba9caaafbb6c84158348961
Huang Yi <coodeer@163.com>
2018-06-10 23:15:15 +0800

    Add method comments.

--- src/Facades/WebsocketNamespace.php
+++ src/Facades/WebsocketNamespace.php
@@ -9,11 +9,12 @@ use Illuminate\Support\Facades\Facade;
  *
  * @method static void join(string $path, int $socketId)
  * @method static void leave(int $socketId)
- * @method static void broadcast(string $path, \HuangYi\Swoole\Contracts\MessageContract $message, array|int|null $excepts)
+ * @method static void broadcast(string $path, \HuangYi\Swoole\Contracts\MessageContract $message, array|int $excepts = null)
  * @method static void emit(int $socketId, \HuangYi\Swoole\Contracts\MessageContract $message)
  * @method static \Swoole\Server getServer()
  * @method static string getPath(int $socketId)
  * @method static array getClients(string $path)
+ * @method static void flushAll()
  * @method static void flush(string $path)
  *
  * @see \HuangYi\Swoole\Websocket\NamespaceManager
--- src/Facades/WebsocketRoute.php
+++ src/Facades/WebsocketRoute.php
@@ -7,6 +7,22 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Websocket Route Facade.
  *
+ * @method static \Illuminate\Routing\Route path(string $uri, \Closure|array|string|null $action = null)
+ * @method static \Illuminate\Routing\RouteRegistrar prefix(string  $prefix)
+ * @method static \Illuminate\Routing\RouteRegistrar middleware(array|string|null $middleware)
+ * @method static \Illuminate\Routing\Route substituteBindings(\Illuminate\Support\Facades\Route $route)
+ * @method static void substituteImplicitBindings(\Illuminate\Support\Facades\Route $route)
+ * @method static \Illuminate\Routing\RouteRegistrar as(string $value)
+ * @method static \Illuminate\Routing\RouteRegistrar domain(string $value)
+ * @method static \Illuminate\Routing\RouteRegistrar name(string $value)
+ * @method static \Illuminate\Routing\RouteRegistrar namespace(string $value)
+ * @method static \Illuminate\Routing\Router|\Illuminate\Routing\RouteRegistrar group(array|\Closure|string $attributes, \Closure|string $routes)
+ * @method static \Illuminate\Routing\Route current()
+ * @method static string|null currentRouteName()
+ * @method static string|null currentRouteAction()
+ * @method static \Illuminate\Routing\Router middlewareGroup(string $name, array $middleware)
+ * @method static \Illuminate\Routing\Router aliasMiddleware(string $name, string $class)
+ *
  * @see \Illuminate\Routing\Router
  */
 class WebsocketRoute extends Facade
