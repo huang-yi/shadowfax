2e81570db28d1b55ab0271ddf153efd2c32a75a4
Huang Yi <coodeer@163.com>
2018-07-30 12:38:45 +0800

    Add method comments

--- src/Facades/WebSocket.php
+++ src/Facades/WebSocket.php
@@ -7,6 +7,15 @@ use Illuminate\Support\Facades\Facade;
 /**
  * WebSocket Facade.
  *
+ * @method static \HuangYi\Swoole\WebSocket\Route room(string $uri, mixed $action = null)
+ * @method static \HuangYi\Swoole\WebSocket\WebSocket joinRoom(\Illuminate\Http\Request $request)
+ * @method static \HuangYi\Swoole\WebSocket\Room getClientRoom(int $socketId)
+ * @method static \HuangYi\Swoole\WebSocket\Room getRoom(string $path)
+ * @method static void emit(int $socketId, \HuangYi\Swoole\Contracts\MessageContract $message)
+ * @method static void broadcast(\HuangYi\Swoole\Contracts\MessageContract $message, mixed $excepts = null)
+ * @method static \HuangYi\Swoole\WebSocket\WebSocket setDefaultRoom(string $uri)
+ * @method static \HuangYi\Swoole\WebSocket\Route on(string $event, mixed $callback)
+ *
  * @see \HuangYi\Swoole\WebSocket\WebSocket
  */
 class WebSocket extends Facade
