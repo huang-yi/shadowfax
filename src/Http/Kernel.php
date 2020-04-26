7b6ce429e09101d23db7dea62333e212d7d0eb2f
huang-yi <coodeer@163.com>
2020-04-26 15:38:34 +0800

    Move 'isWebSocket' parameter to 'handle' method

--- src/Http/Kernel.php
+++ src/Http/Kernel.php
@@ -5,6 +5,7 @@ namespace HuangYi\Shadowfax\Http;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Http\Kernel as IlluminateHttpKernel;
 use Laravel\Lumen\Application as Lumen;
+use Throwable;
 
 class Kernel
 {
@@ -16,49 +17,43 @@ class Kernel
     protected $app;
 
     /**
-     * Indicates if the kernel is processing a WebSocket request.
-     *
-     * @var bool
-     */
-    protected $isWebSocket;
-
-    /**
      * The Swoole Http Kernel.
      *
      * @param  \Illuminate\Contracts\Container\Container  $app
      * @param  bool  $isWebSocket
      * @return void
      */
-    public function __construct(Container $app, $isWebSocket = false)
+    public function __construct(Container $app)
     {
         $this->app = $app;
-        $this->isWebSocket = $isWebSocket;
     }
 
     /**
      * Handle the incoming HTTP request.
      *
      * @param  \HuangYi\Shadowfax\Http\Request  $request
+     * @param  bool $isWebSocket
      * @return \HuangYi\Shadowfax\Http\Response
      */
-    public function handle(Request $request)
+    public function handle(Request $request, bool $isWebSocket = false)
     {
         if ($this->isLumen()) {
-            return $this->runLumen($request);
+            return $this->runLumen($request, $isWebSocket);
         }
 
-        return $this->runLaravel($request);
+        return $this->runLaravel($request, $isWebSocket);
     }
 
     /**
      * Run the Laravel framework.
      *
      * @param  \HuangYi\Shadowfax\Http\Request  $request
+     * @param  bool  $isWebSocket
      * @return \HuangYi\Shadowfax\Http\Response
      */
-    public function runLaravel(Request $request)
+    public function runLaravel(Request $request, $isWebSocket = false)
     {
-        $kernel = $this->getKernel();
+        $kernel = $this->getKernel($isWebSocket);
 
         $illuminateResponse = $kernel->handle(
             $illuminateRequest = $request->toIlluminate()
@@ -76,34 +71,47 @@ class Kernel
     }
 
     /**
-     * Run the Lumen framework.
-     *
-     * @param  \HuangYi\Shadowfax\Http\Request  $request
-     * @return \HuangYi\Shadowfax\Http\Response
-     */
-    public function runLumen(Request $request)
-    {
-        return Response::make(
-            $this->app->handle($request->toIlluminate())
-        );
-    }
-
-    /**
      * Get the HTTP kernel instance.
      *
+     * @param  bool  $isWebSocket
      * @return \Illuminate\Contracts\Http\Kernel
      */
-    protected function getKernel()
+    protected function getKernel($isWebSocket = false)
     {
         $kernel = $this->app[IlluminateHttpKernel::class];
 
-        if (! $this->isWebSocket) {
+        if (! $isWebSocket) {
             return $kernel;
         }
 
         $class = get_class($kernel);
 
         return new $class($this->app, $this->app['shadowfax.websocket']);
+    }
+
+    /**
+     * Run the Lumen framework.
+     *
+     * @param  \HuangYi\Shadowfax\Http\Request  $request
+     * @param  bool  $isWebSocket
+     * @return \HuangYi\Shadowfax\Http\Response
+     */
+    public function runLumen(Request $request, $isWebSocket = false)
+    {
+        if ($isWebSocket) {
+            $router = $this->app->router;
+            $this->app->router = $this->app['shadowfax.websocket'];
+        }
+
+        try {
+            return Response::make(
+                $this->app->handle($request->toIlluminate())
+            );
+        } finally {
+            if ($isWebSocket) {
+                $this->app->router = $router;
+            }
+        }
     }
 
     /**
