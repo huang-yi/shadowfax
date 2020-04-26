6d15539a2e02edc6649914af62ba2e87468cd7dd
huang-yi <coodeer@163.com>
2020-04-26 15:39:43 +0800

    Initialize connection and dispatch open event

--- src/Listeners/HandshakeEvent/HandleHandshake.php
+++ src/Listeners/HandshakeEvent/HandleHandshake.php
@@ -3,9 +3,12 @@
 namespace HuangYi\Shadowfax\Listeners\HandshakeEvent;
 
 use HuangYi\Shadowfax\Events\HandshakeEvent;
+use HuangYi\Shadowfax\Events\OpenEvent;
 use HuangYi\Shadowfax\Http\Kernel;
 use HuangYi\Shadowfax\Http\Request;
 use HuangYi\Shadowfax\Listeners\HasHelpers;
+use HuangYi\Shadowfax\WebSocket\Connection;
+use HuangYi\Shadowfax\WebSocket\RequestVerifier;
 
 class HandleHandshake
 {
@@ -24,26 +27,54 @@ class HandleHandshake
                 $request = Request::make($event->request), true
             );
 
-            $this->formatResponse($response);
+            if ($this->isSuccessful($response)) {
+                Connection::init(shadowfax('server'), $request);
+            }
+
+            $this->formatResponse($request, $response);
 
             $response->send($event->response);
+
+            if ($this->isSuccessful($response)) {
+                shadowfax('events')->dispatch(new OpenEvent(shadowfax('server'), $event->request));
+            }
         });
     }
 
     /**
      * Format the response.
      *
+     * @param  \HuangYi\Shadowfax\Http\Request  $request
      * @param  \HuangYi\Shadowfax\Http\Response  $response
      * @return void
      */
-    protected function formatResponse($response)
+    protected function formatResponse($request, $response)
     {
-        $status = $response->getIlluminateResponse()->getStatusCode();
+        if ($this->isSuccessful($response)) {
+            $verifier = new RequestVerifier($request->getIlluminateRequest());
 
-        if ($status >= 200 && $status < 300) {
+            $response->getIlluminateResponse()->withHeaders([
+                'Upgrade' => 'websocket',
+                'Connection' => 'Upgrade',
+                'Sec-WebSocket-Accept' => $verifier->getSecWebSocketAccept(),
+            ]);
+
             $response->getIlluminateResponse()->setStatusCode(101);
         }
 
         $response->getIlluminateResponse()->setContent('');
+    }
+
+    /**
+     * Determine whether handshake is successful.
+     *
+     * @param  \HuangYi\Shadowfax\Http\Response  $response
+     * @return bool
+     */
+    protected function isSuccessful($response)
+    {
+        $status = $response->getIlluminateResponse()->getStatusCode();
+
+        return $status == 101 || ($status >= 200 && $status < 300);
     }
 }
