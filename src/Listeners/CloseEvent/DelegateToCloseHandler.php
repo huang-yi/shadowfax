8cc657130241b70e8a19a7af036083f1e07d54da
Huang Yi <coodeer@163.com>
2020-07-26 21:52:20 +0800

    Fix close the WebSocket connection

--- src/Listeners/CloseEvent/DelegateToCloseHandler.php
+++ src/Listeners/CloseEvent/DelegateToCloseHandler.php
@@ -5,7 +5,6 @@ namespace HuangYi\Shadowfax\Listeners\CloseEvent;
 use HuangYi\Shadowfax\Events\CloseEvent;
 use HuangYi\Shadowfax\Listeners\HasHelpers;
 use HuangYi\Shadowfax\WebSocket\ConnectionCollection;
-use Swoole\WebSocket\Server;
 
 class DelegateToCloseHandler
 {
@@ -19,14 +18,23 @@ class DelegateToCloseHandler
      */
     public function handle(CloseEvent $event)
     {
-        if (! $event->server instanceof Server) {
+        $this->closeWebSocketConnection($event);
+    }
+
+    /**
+     * Close the WebSocket connection.
+     *
+     * @param  \HuangYi\Shadowfax\Events\CloseEvent  $event
+     * @return void
+     */
+    protected function closeWebSocketConnection(CloseEvent $event)
+    {
+        if (! $connection = ConnectionCollection::find($event->fd)) {
             return;
         }
 
-        $this->handleWithoutException(function () use ($event) {
-            if ($connection = ConnectionCollection::find($event->fd)) {
-                $connection->getHandler()->onClose($connection);
-            }
+        $this->handleWithoutException(function () use ($connection) {
+            $connection->getHandler()->onClose($connection);
         });
 
         ConnectionCollection::forget($event->fd);
